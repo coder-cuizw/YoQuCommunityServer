21e5de0ae6607e71c3598a537c92096703c5b927
XG2475 <862334436yyg@gmail.com>
2019-10-13 14:12:18 +0800

    更新搜索逻辑

--- src/main/java/net/gupt/community/util/UrlUtil.java
+++ src/main/java/net/gupt/community/util/UrlUtil.java
@@ -16,21 +16,30 @@ import java.net.URLDecoder;
  */
 @Slf4j
 public class UrlUtil {
-    public static String deCodeWithRegexp(String str, String charset, String expression) {
+    /**
+     * URL解码并每个字符分割然后格式化
+     *
+     * @param str      字符串
+     * @param charset  字符编码
+     * @param splitter 分割符
+     * @return String
+     */
+    public static String deCodeWithRegexp(String str, String charset, String splitter) {
         StringBuilder builder = new StringBuilder();
         if (!str.trim().isEmpty()) {
             try {
+                //对传进来的字符串进行同一编码，并转换为字符数组
                 String content = URLDecoder.decode(str, charset);
                 char[] chars = content.toCharArray();
-
+                //在每个字符后加一条分割符
                 for (char character : chars
                 ) {
-                    builder.append(character).append(expression);
+                    builder.append(character).append(splitter);
                 }
             } catch (UnsupportedEncodingException e) {
                 throw new RuntimeException(e);
             }
         }
-        return builder.substring(0, builder.lastIndexOf(expression));
+        return builder.substring(0, builder.lastIndexOf(splitter));
     }
 }
