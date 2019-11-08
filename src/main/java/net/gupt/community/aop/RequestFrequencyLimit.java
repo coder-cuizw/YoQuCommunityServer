0955fe8d19d901b583daba73d343338ddc300861
XG2475 <862334436yyg@gmail.com>
2019-11-08 09:34:26 +0800

    请求频繁添加注释

--- src/main/java/net/gupt/community/aop/RequestFrequencyLimit.java
+++ src/main/java/net/gupt/community/aop/RequestFrequencyLimit.java
@@ -36,14 +36,12 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @Aspect
 public class RequestFrequencyLimit {
 
-    private RedisAuth redisAuth;
-    private final Gson gson;
+    private final HttpServletRequest request;
+    private final RedisAuth redisAuth;
 
-
-    public RequestFrequencyLimit(RedisAuth redisAuth, Gson gson) {
+    public RequestFrequencyLimit(RedisAuth redisAuth, HttpServletRequest request) {
+        this.request = request;
         this.redisAuth = redisAuth;
-
-        this.gson = gson;
     }
 
     @Before("within(@org.springframework.web.bind.annotation.RestController *) && @annotation(limit)")
@@ -52,56 +50,23 @@ public class RequestFrequencyLimit {
         jedis.auth(redisAuth.getPassword());
         //切换redis数据库到db1
         jedis.select(1);
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
         Student student = (Student) request.getAttribute("Student");
         String uid = String.valueOf(student.getUid());
         String servletPath = request.getServletPath().replace("/", "|");
         //拼接Key
         String key = uid.concat(servletPath);
+        //将key进行Aes加密
         String encKey = encyKey(key);
         boolean result = incrKey(encKey, jedis, limit);
+        //当结果返回true向页面输出
         if (result) {
             responseResult(response);
         }
     }
 
     /**
-     * 向redis插入数据方法
-     *
-     * @param key   <br/>
-     * @param jedis <br/>
-     * @param limit <br/>
-     * @return boolean
-     */
-    private boolean incrKey(String key, Jedis jedis, LimitFrequency limit) {
-        boolean state = true;
-        if (!key.trim().isEmpty() && jedis != null && limit != null) {
-            jedis.incr(key);
-            if (Integer.parseInt(jedis.get(key)) < limit.count()) {
-                jedis.expire(key, (int) (limit.time() / 1000));
-            }
-            state = !checkByRedis(limit, key, jedis);
-            jedis.close();
-        }
-        return state;
-    }
-
-    /**
-     * 从Redis获取访问次数方法
-     *
-     * @param limit  限制的时间
-     * @param encKey 加密后的key
-     * @return boolean
-     */
-    private boolean checkByRedis(LimitFrequency limit, String encKey, Jedis jedis) {
-        Integer incrByCount = Integer.valueOf(jedis.get(encKey));
-        //如果redis的计数大于设置的次数，如果返回false，表示频繁请求
-        return incrByCount <= limit.count();
-    }
-
-    /**
-     * 向前端响应方法
+     * 向前端响应
      *
      * @param response <br/>
      */
@@ -110,14 +75,49 @@ public class RequestFrequencyLimit {
             response.setHeader("Content-Type", "application/json;charset=UTF-8");
             try {
                 OutputStream writer = response.getOutputStream();
-                writer.write(gson.toJson(Result.error(CodeMsg.REQUEST_FREQUENT)).getBytes(UTF_8));
+                writer.write(new Gson().toJson(Result.error(CodeMsg.REQUEST_FREQUENT)).getBytes(UTF_8));
                 writer.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            //阻塞请求
+            //中断程序
             throw new RuntimeException("请求过于频繁，超出限制");
         }
+    }
+
+    /**
+     * 向redis插入数据方法
+     *
+     * @param key   加密的键
+     * @param jedis redis操作对象
+     * @param limit <br/>
+     * @return boolean
+     */
+    private boolean incrKey(String key, Jedis jedis, LimitFrequency limit) {
+        boolean state = false;
+        if (!key.trim().isEmpty() && jedis != null && limit != null) {
+            jedis.incr(key);
+            //如果获取到Redis的次数小于设置的次数，设置过期时间
+            if (Integer.parseInt(jedis.get(key)) < limit.count()) {
+                jedis.expire(key, (int) (limit.time() / 1000));
+            }
+            //请求频繁返回false
+            state = !checkByRedis(limit, key, jedis);
+            jedis.close();
+        }
+        return state;
+    }
+
+    /**
+     * 从Redis获取访问次数
+     *
+     * @param limit  限制的时间
+     * @param encKey 加密后的key
+     * @return boolean
+     */
+    private boolean checkByRedis(LimitFrequency limit, String encKey, Jedis jedis) {
+        Integer incrByCount = Integer.valueOf(jedis.get(encKey));
+        return incrByCount <= limit.count();
     }
 
     /**
